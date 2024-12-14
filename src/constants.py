d1033072799eeb3f7e05ebb673571b13a9207a99
Samwel Emmanuel <samakubi@gmail.com>
2024-12-14 18:02:59 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 OPENAI_API_KEY = "sk-IE1WdGWqTxUPCGqo8YPR2_YiX2jA6kt4H2j-JnidzZT3BlbkFJnkU4fahAPFuTL0OTXDwZeAHtod7RfZ5STTE3EH5PwA"
-LLM_MODEL_NAME = "gpt-4-0125-preview"
+LLM_MODEL_NAME = "gpt-3.5-turbo"
 USER = "root"
 PASSWORD = "MamaGrentina$100!"
 HOST = "database-1.c7ew0quossbs.us-east-1.rds.amazonaws.com"
