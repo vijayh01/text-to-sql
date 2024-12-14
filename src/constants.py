2f1c5d2f3b86d6ae4c9b4b8a2310177d3e1a8149
Samwel Emmanuel <samakubi@gmail.com>
2024-12-14 17:43:16 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,7 @@
-OPENAI_API_KEY = "YOUR_KEY"
+OPENAI_API_KEY = "sk-FT5FTwTgB7__D52k87AyPpowXfeDDeGFaeR5O-zzART3BlbkFJGtEAC2HUjUyBBQOK0DGRQ4XJCcvvQd41XAZuGSWnMA"
 LLM_MODEL_NAME = "gpt-4-0125-preview"
+USER = "root"
+PASSWORD = "MamaGrentina$100!"
+HOST = "database-1.c7ew0quossbs.us-east-1.rds.amazonaws.com"
+DATABASE = "ecommerce"
+PORT = 3306
