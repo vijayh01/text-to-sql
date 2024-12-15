e045cd738375fc93040219dc16dd023b0ac90e97
Samwel Emmanuel <samakubi@gmail.com>
2024-12-14 20:36:11 -0500

    More stochastic changes to page config

--- src/app.py
+++ src/app.py
@@ -26,7 +26,7 @@ sys.path.insert(0, parent_dir)
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 
 # Configure Streamlit page
-st.set_page_config(page_title="Ecommerce SQL Agent", layout="wide")
+st.set_page_config(page_title="Ecommerce SQL Agent")
 
 # Initialize session state
 if 'agent_memory' not in st.session_state:
