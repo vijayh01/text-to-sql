7bdbcf7d2135f8c626357bd6ae0a77645b958718
Samwel Emmanuel <samakubi@gmail.com>
2024-12-14 19:08:00 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 OPENAI_API_KEY = "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"
-LLM_MODEL_NAME = "o1-mini"
+LLM_MODEL_NAME = "gpt-4-0125-preview"
 USER = "root"
 PASSWORD = "MamaGrentina$100!"
 HOST = "database-1.c7ew0quossbs.us-east-1.rds.amazonaws.com"
