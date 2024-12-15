729c3f393761192fbe3759a583fc395b0b5c7803
Samwel Emmanuel <samakubi@gmail.com>
2024-12-14 19:02:37 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = "sk-IE1WdGWqTxUPCGqo8YPR2_YiX2jA6kt4H2j-JnidzZT3BlbkFJnkU4fahAPFuTL0OTXDwZeAHtod7RfZ5STTE3EH5PwA"
+OPENAI_API_KEY = "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"
 LLM_MODEL_NAME = "gpt-3.5-turbo"
 USER = "root"
 PASSWORD = "MamaGrentina$100!"
