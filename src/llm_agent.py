bbccbfd09cbf6da32cb38f2cf18fd63164daa59d
Samwel Emmanuel <samakubi@gmail.com>
2024-12-30 16:12:42 -0500

    the zone

--- src/llm_agent.py
+++ src/llm_agent.py
@@ -99,17 +99,6 @@ def initialize_python_agent(agent_llm_name: str = LLM_MODEL_NAME):
     Returns:
         AgentExecutor: An agent executor configured for Python-related tasks.
     """
-    # instructions = """You are an agent designed to write python code to answer questions.
-    #         You have access to a python REPL, which you can use to execute python code.
-    #         If you get an error, debug your code and try again.
-    #         You might know the answer without running any code, but you should still run the code to get the answer.
-    #         If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
-    #         Always output the python code only.
-    #         Generate the code <code> for plotting the previous data in plotly, in the format requested. 
-    #         The solution should be given using plotly and only plotly. Do not use matplotlib.
-    #         Return the code <code> in the following
-    #         format ```python <code>```
-    #         """
     instructions = """You are an agent designed to write python code to answer questions.
     You have access to a python REPL to execute code.
     If you get an error, debug your code and try again.
@@ -122,8 +111,17 @@ def initialize_python_agent(agent_llm_name: str = LLM_MODEL_NAME):
     """
     tools = [PythonREPLTool()]
     base_prompt = hub.pull("langchain-ai/openai-functions-template")
-    prompt = base_prompt.partial(instructions=instructions)
-    agent = create_openai_functions_agent(ChatOpenAI(model=agent_llm_name, temperature=0), tools, prompt)
+    # prompt = base_prompt.partial(instructions=instructions)
+    # agent = create_openai_functions_agent(ChatOpenAI(model=agent_llm_name, temperature=0), tools, prompt)
+
+    prompt_messages = base_prompt.format_prompt(instructions=instructions).to_messages()
+    combined_prompt = ChatPromptTemplate.from_messages(prompt_messages)
+    agent = create_openai_functions_agent(
+        ChatOpenAI(model=agent_llm_name, temperature=0),
+        tools,
+        combined_prompt
+    )
+
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     return agent_executor
 
