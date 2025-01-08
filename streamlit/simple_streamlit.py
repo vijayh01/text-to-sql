34ddedc84e8b3515a85d4ac5aefdf607e4e377b8
Samwel Emmanuel <samakubi@gmail.com>
2025-01-08 00:51:54 -0500

    added chat input

--- streamlit/simple_streamlit.py
+++ streamlit/simple_streamlit.py
@@ -5,5 +5,8 @@ st.set_page_config(page_title="SQL and Python Agent")
 # Main Page Title
 st.title("SQL and Python Agent")
 
-# sidebar
+# Sidebar
 st.sidebar.title("DATABASE CONFIGURATION")
+
+# Chat Input
+st.chat_input("Please ask your question:")
