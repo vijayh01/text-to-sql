bcabbf4e9c24d69bf62528279fcde12fc3bf9906
Samwel Emmanuel <samakubi@gmail.com>
2025-01-16 22:43:55 -0500

    test

--- streamlit/simple_streamlit.py
+++ streamlit/simple_streamlit.py
@@ -4,12 +4,12 @@ st.set_page_config(page_title="SQL and Python Agent")
 # MAIN PAGE
 st.title("SQL and Python Agent")
 
-# SIDE BAR
-st.sidebar.title("DATABASE CONFIGURATION")
-st.sidebar.subheader("Enter MySQL connection details:", divider=True)
+# # SIDE BAR
+# st.sidebar.title("DATABASE CONFIGURATION")
+# st.sidebar.subheader("Enter MySQL connection details:", divider=True)
 
 
-# CHAT INPUT
-if prompt := st.chat_input("Please ask your question:"):
-   with st.chat_message("user", avatar="🚀"):
-     st.markdown(prompt)
+# # CHAT INPUT
+# if prompt := st.chat_input("Please ask your question:"):
+#    with st.chat_message("user", avatar="🚀"):
+#      st.markdown(prompt)
