6bbde7712dbad21f19470c3b6a79359dcea526aa
Samwel Emmanuel <samakubi@gmail.com>
2025-01-16 22:48:05 -0500

    sidebar

--- streamlit/simple_streamlit.py
+++ streamlit/simple_streamlit.py
@@ -4,9 +4,12 @@ st.set_page_config(page_title="SQL and Python Agent")
 # MAIN PAGE
 st.title("SQL and Python Agent")
 
-# # SIDE BAR
-# st.sidebar.title("DATABASE CONFIGURATION")
-# st.sidebar.subheader("Enter MySQL connection details:", divider=True)
+
+
+
+# SIDE BAR
+st.sidebar.title("DATABASE CONFIGURATION")
+st.sidebar.subheader("Enter MySQL connection details:", divider=True)
 
 
 # # CHAT INPUT
